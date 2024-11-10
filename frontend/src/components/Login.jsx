3aab63851abee64646ef2d31d4298db596f60cf0
Dinesh Gupta <161644718+987Learner108@users.noreply.github.com>
2024-11-10 17:37:11 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -16,7 +16,7 @@ function Login() {
 
         try{
 
-            await axios.post("online-quiz-app-using-react-api.vercel.app",{
+            await axios.post("online-quiz-app-using-react-kr5b.vercel.app/",{
                 email,password
             })
             .then(res=>{
